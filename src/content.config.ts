9120ee10c34fbf42d7c29f5bd6435723867bbe6a
Thimo Thorandt <87324856+BirnB4um@users.noreply.github.com>
2025-11-24 19:18:50 +0100

    added support for blog/project posts

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,11 @@
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
+const zDateDMY = z.string().transform((str) => {
+  const [day, month, year] = str.split(".").map(Number);
+  return new Date(year, month - 1, day);
+});
+
 const blog = defineCollection({
 	// Load Markdown and MDX files in the `src/content/blog/` directory.
 	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
@@ -8,12 +13,24 @@ const blog = defineCollection({
 	schema: ({ image }) =>
 		z.object({
 			title: z.string(),
-			description: z.string(),
-			// Transform string to Date object
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			heroImage: image().optional(),
+			description: z.string().optional(),
+			pubDate: zDateDMY,
+			updatedDate: zDateDMY.optional(),
+			thumbnail: image(),
 		}),
 });
 
-export const collections = { blog };
+const projects = defineCollection({
+	loader: glob({ base: './src/content/projects', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string().optional(),
+			pubDate: zDateDMY,
+			updatedDate: zDateDMY.optional(),
+			thumbnail: image(),
+		}),
+});
+
+export const collections = { blog, projects };
