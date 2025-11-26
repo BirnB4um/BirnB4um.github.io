3b34aa5c60e9fe7b7359ace23f684601ca2c8771
Thimo Thorandt <87324856+BirnB4um@users.noreply.github.com>
2025-11-26 11:32:32 +0100

    added titles and descriptions

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'BirnB4um\'s Archive';
+export const SITE_DESCRIPTION = 'Welcome to my website! A place where I share my projects, experiments, and thoughts.';
