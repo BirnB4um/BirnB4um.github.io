09f5e4b3bd778d819322b4189d74bc958f6b35bf
Thimo Thorandt <87324856+BirnB4um@users.noreply.github.com>
2025-11-27 19:07:33 +0100

    removed blog, added home & about

--- src/content.config.ts
+++ src/content.config.ts
@@ -6,20 +6,6 @@ const zDateDMY = z.string().transform((str) => {
   return new Date(year, month - 1, day);
 });
 
-const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string().optional(),
-			pubDate: zDateDMY,
-			updatedDate: zDateDMY.optional(),
-			thumbnail: image().optional(),
-		}),
-});
-
 const projects = defineCollection({
 	loader: glob({ base: './src/content/projects', pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
@@ -33,4 +19,4 @@ const projects = defineCollection({
 		}),
 });
 
-export const collections = { blog, projects };
+export const collections = {  projects };
